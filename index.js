97944859c7cad72fa101f6fbfa889a2da0063fae
Santino Exequiel Pilot <santinoexequielpilot0@gmail.com>
2026-02-21 17:47:08 -0300

    feat: agregar conexion a base de datos y encender la app solo si la conexion fue exitosa

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 import app from "./src/app.js"
+import pool from "./src/config/database.js"
 
-const port = 3000
+const iniciarServidor = async () => {
+    try {
+        await pool.query("SELECT NOW()")
+        console.log("La conexion a la DB fue exitosa")
 
-app.listen(port, () => {
-    console.log(`La app esta funcionando en el puerto ${port}`)
-})
+        const port = 3000
+
+        app.listen(port, () => {
+            console.log(`La app esta funcionando en el puerto ${port}`)
+        })
+
+    } catch (error) {
+        console.error("Error en la DB:", error.message)
+        process.exit(1)
+    }
+}
+
+iniciarServidor()
