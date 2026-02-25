c05feb7dd653bded86f8a81cbaec4996b4246e2e
Santino Exequiel Pilot <santinoexequielpilot0@gmail.com>
2026-02-25 14:40:26 -0300

    feat: limitar las peticiones al endpoint cargarEmpleados

--- src/routes/empleado.route.js
+++ src/routes/empleado.route.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import { cargarEmpleados, obtenerEmpleados } from "../controllers/empleado.controller.js";
 import uploadCsv from "../middlewares/uploadCsv.middleware.js";
+import rateLimit from "express-rate-limit";
 
 const router = Router()
 
-router.post("/importar", uploadCsv.single("archivo"), cargarEmpleados)
+const limiter = rateLimit({
+    windowMs: 60 * 60 * 1000,
+    max: 5, 
+    message: {success: false, message: "Demasiados intentos, reintenta en 1 hora"}
+})
+
+router.post("/importar", limiter, uploadCsv.single("archivo"), cargarEmpleados)
 router.get("/", obtenerEmpleados)
 export default router
