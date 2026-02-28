c26282e551c6932cb9b8193f33ba43d70e179580
Santino Exequiel Pilot <santinoexequielpilot0@gmail.com>
2026-02-28 17:19:31 -0300

    fix: arreglar el cors y terminar todo para el deploy

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,13 +6,13 @@ import cors from "cors"
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({ origin: process.env.FRONTEND_URL || "http://localhost:5173" }))
 
 app.use("/empleados", empleadoRouter)
 
 // Funcion que responde a una peticion hacia un endpoint que no existe 
 app.use((req, res) => {
-    return res.status(404).json({ 
+    return res.status(404).json({
         success: false,
         message: "Endpoint no encontrado"
     })
--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -6,9 +6,12 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    max: 10, 
+    max: 10,
     idleTimeoutMillis: 30000, // Cierra las conexiones inactivas despues de 30 segundos
     connectionTimeoutMillis: 2000, // Tiempo maximo de 2 segundos para esperar una conexion disponible
+    ssl: {
+        rejectUnauthorized: false,
+    }
 })
 
 export default pool
